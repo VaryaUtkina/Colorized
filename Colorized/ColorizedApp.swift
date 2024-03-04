aba82c1fa1a2963e20a47691031a82590d0a8e80
Varya Utkina <varya2412@mail.ru>
2024-03-04 20:41:16 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Варвара Уткина on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
